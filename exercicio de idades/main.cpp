94b7c79fce0db8a7f0cfa231401595b61ae4e8c8
Bruno de Almeida Ferreira Mendes <109405514+Brunoafms@users.noreply.github.com>
2022-10-30 15:54:03 -0300

    Add files via upload

---
+++ exercicio de idades/main.cpp
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+
+int main() {
+    float idade,idade2,soma;
+     printf("Dados da primeira pessoa:  ");
+ scanf("%f",&idade);
+       printf("Dados da segunda pessoa:  ");
+ scanf("%f",&idade2);
+    soma=(idade+idade2)/2;
+       printf("idade media das somas:%.2f ",soma);
+         return 0;
+}
